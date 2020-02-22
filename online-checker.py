e0ef7209b3c9d128d577bf7b4d28a64a74662ad2
vaferrr <likvidoff@gmail.com>
2020-02-22 15:27:47 +0300

    Test #1
    A commit to test this code on termux.

---
+++ online-checker.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import time
+import argparse
+
+parser = argparse.ArgumentParser('python online-checker.py id123456789')
+parser.add_argument('id', help='Paste here the user ID. ID format: id123456789 or other. !!!DO NOT PASTE A LINK!!!')
+args = parser.parse_args()
+
+online = ''
+
+while online is not 'Online':
+
+    r = requests.get('https://vk.com/' + args.id)
+    html = bs(r.content, 'html.parser')
+    online = html.select('.pp_last_activity_text')
+    print(online[0].text)
+
+    if online[0].text == 'Online':
+        termux-notification --button1 'The user is Online!' --sound
+        break
+    else:
+        pass
+    
+    time.sleep(5)
